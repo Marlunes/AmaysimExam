0cdda94cdf54e4b1d69865d3d9868f2cd327ea57
Marlon Boncodin <boncodin.marlon@gmail.com>
2017-06-06 21:39:50 +0800

    Initial Commit

---
+++ Amaysim/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Amaysim
+//
+//  Created by Marlon Boncodin on 06/06/2017.
+//  Copyright © 2017 Marlon Boncodin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
